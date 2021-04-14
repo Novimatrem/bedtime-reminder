49f07ee3de610560a5090adeb95f570292f47df5
Novimatrem <thenovimatrem@protonmail.ch>
2021-04-14 00:45:30 +0000

    improve optional nighttime shutdown

--- noisemaker.sh
+++ noisemaker.sh
@@ -108,7 +108,10 @@ echo ""
 killall espeak
 killall speech-dispatcher
 
+#optional nighttime shutdown, uncomment to use
 #sleep 300s
+#dbus-send --system --print-reply --dest=org.freedesktop.login1 /org/freedesktop/login1 "org.freedesktop.login1.Manager.PowerOff" boolean:true
+#sleep 15s
 #init 0
 
 exit
